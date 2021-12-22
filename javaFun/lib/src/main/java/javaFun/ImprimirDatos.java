f4c8e7a8b596c515bba68c6c1d9f6bf37f7ec1a3
Carlos Miguel Zárate Pereira <czaratep@emeal.nttdata.com>
2021-12-22 16:23:34 +0100

    Se realizaron los dos primeros ejercicios de la semana 1

---
+++ javaFun/lib/src/main/java/javaFun/ImprimirDatos.java
@@ -0,0 +1,11 @@
+package javaFun;
+
+public class ImprimirDatos {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Mi nombre es Carlos" + "\n" + "\n" + "Tengo 18 años de edad" + "\n" + "\n" + "Mi ciudad es Penco, Chile");
+
+	}
+
+}

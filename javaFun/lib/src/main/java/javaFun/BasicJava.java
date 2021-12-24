94a4f8624a5a29bd5771377243eaccf6f079aace
Carlos Miguel Zárate Pereira <czaratep@emeal.nttdata.com>
2021-12-24 12:02:25 -0300

    Se añadieron las Actividades: BasicJava y StringManipulator

---
+++ javaFun/lib/src/main/java/javaFun/BasicJava.java
@@ -0,0 +1,146 @@
+package javaFun;
+
+import java.util.ArrayList;
+
+public class BasicJava {
+
+	public static void main(String[] args) {
+	
+		//Imprimir 1 - 255
+		System.out.println("Imprimir 1 - 255");
+		
+		int [] num1 = new int [256];
+		
+		for (int i=1 ; i <= 255 ; i++) {
+			num1 [i] = i;
+			System.out.println(num1 [i]);
+		}
+
+		//Imprimir números impares entre 1 - 255
+		System.out.println("\n" + "Imprimir números impares entre 1 - 255");
+				
+		int [] num2 = new int [256];
+		
+		for (int i=1 ; i <= 255 ; i++) {
+			num2 [i] = i;
+			if (num2 [i] % 2 != 0) {	
+				System.out.println(num2[i]);
+			}
+		}
+		
+		//Imprimir la suma
+		System.out.println("\n" + "Imprimir la suma");
+		
+		int suma1 = 0;
+		
+		for (int i=0 ; i <= 255 ; i++) {
+			suma1 = i + suma1;
+			System.out.println("Nuevo número: " + i + " " + "suma: " + suma1);
+		}
+		
+		//Recorre un arreglo
+		System.out.println("\n" + "Recorrer un arreglo");
+		
+		int [] x = {1,3,5,7,9,13};
+		
+		for (int i=0 ; i < x.length ; i++) {
+			System.out.println(x[i]);
+		}
+		
+		//Encontrar el Máximo
+		System.out.println("\n" + "Encontrar el máximo");
+		
+		int[] x2 = {-3,-5,-7,-8,-4,-2};
+		int max = x2[0];
+ 
+		for(int i = 0; i < x2.length; i++){
+			if(max < x2[i])
+			{
+				max=x2[i];
+			}
+		}
+		System.out.println("El máximo es " + max);
+		
+		//Obtener el promedio
+		System.out.println("\n" + "Obtener el promedio");
+		
+		int[] num4 = {2,10,3};
+		int suma2 = 0;
+		int prom;
+		
+		for(int i=0 ; i < num4.length ; i++) {
+			suma2 = num4[i] + suma2;
+		}
+		prom =suma2 / num4.length;
+		System.out.println("El promedio es: " + prom);
+		
+		//Arreglo de números impares
+		System.out.println("\n" + "Arreglo de números impares");
+		
+		ArrayList<Integer> y = new ArrayList <Integer>();
+		
+		for(int i=0 ; i <= 255 ; i++) {
+			if (i % 2 != 0) {
+				y.add(i);
+			}
+		}
+		System.out.println(y);
+		
+		//Mayor que Y
+		System.out.println("\n" + "Mayor que Y");
+		
+		int[] num5 = {1,3,5,7};
+		int y2 = 3;
+		int result = 0;
+		
+		for(int i=1 ; i < num5.length ; i++)
+		if(num5[i] > y2) {
+			result = result + 1;	
+		}
+		System.out.println(result);
+		
+		//Valores al cuadrado
+		System.out.println("\n" + "Valores al cuadrado");
+		
+		int[] x3 = {1,5,10,-2};
+		
+		for(int i = 0 ; i < x3.length ; i++) {
+			x3[i] = x3[i] * x3[i];
+			System.out.println(x3[i]);
+		}
+		
+		//Eliminar números negativos
+		System.out.println("\n" + "Eliminar números negativos");
+		
+		int[] x4 = {1,5,10,-2};
+		
+		for(int i = 0 ; i < x4.length ; i++) {
+			if(x4[i] < 0) {
+				x4[i] = 0;
+			}
+			System.out.println(x4[i]);
+		}
+	
+		//Mínimo, Máximo y Promedio
+		System.out.println("\n" + "Mínimo, Máximo y Promedio");
+		
+		int[] x5 = {1,5,10,-2};
+		ArrayList<Integer> resultado = new ArrayList<Integer>();
+		int max1 = 0, min1 = 0, suma3 = 0, prom1;
+		
+		for(int i = 0 ; i < x5.length ; i++) {
+			suma3 = x5[i] + suma3;
+			if(max1 < x5[i]) {
+				max1 = x5[i];
+			}
+			if(min1 > x5[i]) {
+				min1 = x5[i];
+			}	
+		}
+		prom1= suma3 / x5.length;
+		resultado.add(min1);
+		resultado.add(max1);
+		resultado.add(prom1);
+		System.out.println(resultado);
+	}
+}
---
+++ javaFun/lib/src/main/java/javaFun/StringManipulator.java
@@ -0,0 +1,97 @@
+package javaFun;
+
+public class StringManipulator {
+	
+	//Metodo trim
+	String trimAndConcat(String string, String string2) {
+		
+		String trim = string.trim() + string2.trim();
+		return trim;
+	}
+	
+	//Metodo indexof
+	int getIndexOrNull(String string, char Char) {
+		
+		int index = string.indexOf(Char);
+		return index;
+	}
+	
+	//Metodo indexof 2
+	int getIndexOrNull2(String string1, String string2) {
+		
+		int index = string1.indexOf(string2);
+		return index;
+	}
+	
+	//Metodo Substring
+	String concatSubstring(String string1, int Int1, int Int2, String string2) {
+		
+		String substring1 = string1.substring(Int1,Int2);
+		String concat = substring1.concat(string2);
+		return concat;
+	}
+
+	public static void main(String[] args)  {
+		
+		//Concatenar Hola Mundo
+		System.out.println("\n");
+		
+		StringManipulator manipulator = new StringManipulator();
+
+		String str = manipulator.trimAndConcat("    Hola     ","     Mundo    ");
+
+		System.out.println(str); // HolaMundo 
+		
+		
+		//Obtener el indice del caracter pedido
+		System.out.println("\n");
+		
+		StringManipulator manipulator1 = new StringManipulator();
+
+		char letter = 'n';
+
+		int a = manipulator1.getIndexOrNull("NTT Data", letter);
+
+		int b = manipulator1.getIndexOrNull("Hola Mundo", letter);
+
+		int c = manipulator1.getIndexOrNull("Saludar", letter);
+
+		System.out.println(a); // 0
+
+		System.out.println(b); // 7
+
+		System.out.println(c); // -1
+
+		
+		//Obtener el indice donde comienza la subcadena
+		System.out.println("\n");
+		
+		StringManipulator manipulator2 = new StringManipulator();
+
+		String word = "Hola";
+
+		String subString = "la";
+
+		String notSubString = "mundo";
+
+		int a2 = manipulator2.getIndexOrNull2(word, subString);
+
+		int b2 = manipulator2.getIndexOrNull2(word, notSubString);
+
+		System.out.println(a2); // 2
+
+		System.out.println(b2); // -1
+		
+		
+		//Obetener una subcadena utilizando un indice de inicio y un indice de finalización
+		System.out.println("\n");
+		
+		StringManipulator manipulator3 = new StringManipulator();
+
+		String word2 = manipulator3.concatSubstring("Hola", 1, 3, "mundo");
+
+		System.out.println(word2); // olmundo
+
+
+	}
+}
